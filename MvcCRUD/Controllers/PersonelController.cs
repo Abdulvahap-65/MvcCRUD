670566cda0988fb30f4bb1b7f8e3f8162bc80a76
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2021-09-19 14:55:36 +0300

    *Asp .NET MVC'de EntityFramework kullanılarak CRUD  İşlemleri gerçekleştirildi.

--- MvcCRUD/Controllers/PersonelController.cs
+++ MvcCRUD/Controllers/PersonelController.cs
@@ -115,6 +115,7 @@ namespace MvcCRUD.Controllers
             return RedirectToAction("Index");
         }
 
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
